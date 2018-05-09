a08f3387390f669935e32734a96c2941d574b1e2
Lee Warrick <leewarrick@gmail.com>
2018-05-08 20:36:09 -0400

    redo wireframe

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+$('.pointer').click(function(event) {
+    $('.blind').hide('blind');
+    event.stopPropagation();
+    let target = $(event.target).attr('data-target');
+    $(target).show('blind');
+})
+
+$('.pointer').hover(
+    function() {
+        $(this).children('.drawerDiv').show('blind');
+    },
+    function() {
+        $(this).children('.drawerDiv').hide('blind');
+    }
+);
