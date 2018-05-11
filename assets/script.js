76a51d94ff7536dc65b5a9a634bb2bb6b816eff6
Lee Warrick <leewarrick@gmail.com>
2018-05-10 21:09:25 -0400

    add style

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
 $('.pointer').click(function(event) {
-    $('.blind').hide('blind');
+    $('.blind').fadeOut();
+    $('.card').hide();                    
     event.stopPropagation();
     let target = $(event.target).attr('data-target');
-    $(target).show('blind');
+    let test = $(target).children('.card');
+    $(target).show('blind', ()=>{
+        chainIn(test, 0);
+    });
 })
 
 $('.pointer').hover(
@@ -14,6 +18,27 @@ $('.pointer').hover(
     }
 );
 
-$(window).click(function() {
-    $('.drawerDiv').hide('blind');
+$(window).click(function(event) {
+    let tagList = [];
+    let arr1 = $(event.target)
+    let tag = arr1[0].tagName.toLowerCase();
+    tagList.push(tag);
+    arr = $(event.target).parents()
+    for(i = 0; i< arr.length; i++) {
+        tagList.push(arr[i].tagName.toLowerCase());
+    }
+    if(tagList.indexOf('section') == -1) {
+        $('.blind').fadeOut();
+        $('.card').hide();                
+    }
 })
+
+function chainIn (arr, current) {
+    if(current == arr.length - 1) {
+        $(arr[current]).fadeIn(500);
+    } else {
+        $(arr[current]).fadeIn(500, function(){
+            chainIn(arr, current + 1)
+        })
+    }
+}
