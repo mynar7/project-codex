debebd0847fdfc7187f4a787218ef83e327dc02b
Reinaldo Llano <llanoreinaldo@gmail.com>
2018-05-18 17:50:33 -0400

    remove php files

--- thankyou.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-Thank you for your message <?php echo $_POST["name"]; ?>!<br>
-We look forward to following up on your request for information within the next 24 hours.
-Your email address is: <?php echo $_POST["email"]; ?>.
-
-</body>
-</html>
